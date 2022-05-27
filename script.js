ca9d07cf1ee6f281e5fe63637ec8a0d6bcbe8ca7
Nick Aleksapolskiy <trend22@mail.ru>
2022-05-27 22:41:54 +0300

    Lesson01 completed

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "Lesson01";
+const screens = "picture.jpeg";
+const screenPrice = 100;
+const rollback = "canсel";
+const fullPrice = 1000;
+const adaptive = "ok";
+
+alert("Внимание! поехали дальше.");
+
+console.log("урок №1!")
