c6f5a3748abb1c4eb06b984900a6690edd99ed37
Nick Aleksapolskiy <trend22@mail.ru>
2022-06-12 16:17:11 +0300

    lesson12 completed исправил в усложнёнке вывод стоимости за вычетом отката rollback

--- js/script.js
+++ js/script.js
@@ -68,27 +68,12 @@ const appData = {
       appData.showResult()
     }
     appData.setBtnEnabled()
-
-    // appData.asking();
-    // //считаем стоимости
-    // appData.getPrices();
-    // //получаем полную стоимость без вычита отката
-    // appData.getFullPrice();
-    // //итоговая стоимость минус сумма отката
-    // appData.getServicePercentPrices();
-    // //получение нового title
-    // appData.getTitle();
-    // //вывод в консоль
     // appData.logger();
-    // console.log(appData)
   },
   //метод для считывания типов экранов и их количества и записи в свойство screens объекта appData
   addScreens: function () {
     //переопределение массива screen
     screens = document.querySelectorAll('.screen')
-    // for (let i = 0; i < screens.length; i++) {
-    //   screens[i].addEventListener('mouseenter', appData.setBtnEnabled)
-    // }
     screens.forEach(function (screen, id) {
       const select = screen.querySelector('select')
       const input = screen.querySelector('input')
@@ -146,8 +131,7 @@ const appData = {
 
     appData.fullPrice = appData.screenPrice + appData.servicePriceNumber + appData.servicePricePercent
 
-    appData.serviceWithRollback = Math.ceil(
-      appData.fullPrice - appData.fullPrice * (appData.rollback / 100))
+    appData.getServiceWithRollback();
 
     //подсчёт общего количества экранов
     appData.screenCounts = appData.screens.reduce(function (sum, item) {
@@ -187,117 +171,126 @@ const appData = {
     inputRangeValue.value = event.target.value
     inputRangeValue.textContent = event.target.value + '%'
     appData.rollback = inputRangeValue.value
+    appData.getServiceWithRollback();
   },
 
-  //метод для определения дополнительных сервисов в числах
-  //   addServicesNumber: function () {
-  //     otherItemsNumber.forEach(function (item) {
-  //       const check = item.querySelector('input[type=checkbox]')
-  //       const label = item.querySelector('label')
-  //       const input = item.querySelector('input[type=text]')
+  getServiceWithRollback: function () {
+    appData.serviceWithRollback = Math.ceil(
+      appData.fullPrice - appData.fullPrice * (appData.rollback / 100))
+    appData.showResult()
+  },
 
-  //       console.log(check)
-  //       console.log(label)
-  //       console.log(input)
-  //     })
-  //     console.log(otherItemsPercent)
-  //   },
-  //метод запроса первоначальных данных
-  //   asking: function () {
-  //запрос наименования проекта и его проверка на string
-  // do {
-  //   appData.title = prompt('Как называется Ваш проект?')
-  // } while (!appData.isString(appData.title))
-
-  //цикл for используется для получения информации о типах экранов
-  // for (let i = 0; i < 2; i++) {
-  //   let name
-  //   let answerPrice = 0
-  //   //запрос наименований экранов и их проверка на string
-  //   do {
-  //     name = prompt('Какие типы экранов нужно разработать?')
-  //   } while (!appData.isString(name))
-  //   //запрос на стоимость разработок экранов и проверка на number
-  //   do {
-  //     answerPrice = prompt('Сколько будет стоить данная работа?')
-  //   } while (!appData.isNumber(answerPrice))
-  //   answerPrice = Number(answerPrice)
-  //   //наполнение массива данных об экранах
-  //   this.screens.push({ id: 1, name: name, price: answerPrice })
-  // }
-
-  //цикл for используется для получения информации о дополнительных сервисах
-  //     for (let i = 0; i < 2; i++) {
-  //       let answerPrice = 0
-  //       let name
-  //       //запрос наименования доуслуги и его проверка на string
-  //       do {
-  //         name = prompt('Какой дополнительный тип услуги №1 нужен?')
-  //       } while (!appData.isString(name))
-  //       //запрос на стоимость допуслуг и проверка на number
-  //       do {
-  //         answerPrice = prompt('Сколько это будет стоить?')
-  //       } while (!appData.isNumber(answerPrice))
-  //       answerPrice = Number(answerPrice)
-  //       //наполнение объекта данными о допуслугах
-  //       appData.services[name + i] = answerPrice
-  //     }
-  //   },
-
-  //getPrices - метод отвечающий за расчёты цен
-
-  // //метод проверки на number
-  // isNumber: function (num) {
-  //   return !isNaN(parseFloat(num)) && isFinite(num)
-  // },
-
-  // //метод проверки на string
-  // isString: function (str) {
-  //   return typeof str === 'string' && isNaN(Number(str))
-  // },
-
-
-
-  //   //метод получения отредактированного названия проекта
-  //   getTitle: function () {
-  //     let firstCharBig
-
-  //     do {
-  //       appData.title = appData.title.trim()
-  //     } while (appData.title.indexOf(' ') === 0)
-
-  //     appData.title = appData.title.toLowerCase()
-  //     //первая буква в строке переводится в большой регистр
-  //     firstCharBig = appData.title[0].toUpperCase()
-  //     // методом replace изменяется первая строка title на firstCharBig
-  //     appData.title = appData.title.replace(appData.title[0], firstCharBig)
-  //   },
-
-
-
-  //метод вычисления скидки
-  // getRollBackMesssage: function (price) {
-  //   switch (true) {
-  //     case price >= 30000:
-  //       return 'Даем скидку в 10%'
-  //       break
-  //     case price >= 15000 && price < 30000:
-  //       return 'Даем скидку в 5%'
-  //       break
-  //     case price >= 0 && price < 15000:
-  //       return 'Скидка не предусмотрена'
-  //       break
-  //     case price < 0:
-  //       return 'Что то пошло не так'
-  //       break
-  //   }
-  // },
-
-  // logger: function () {
-  //   console.log(appData.fullPrice)
-  //   console.log(appData.servicePercentPrice)
-  // },
 }
 
 //------------запуск приложения-------------------
 appData.init()
+//--------------------------------------------------------------------------------------------
+//----------------- возможно понадобится -----------
+//метод для определения дополнительных сервисов в числах
+//   addServicesNumber: function () {
+//     otherItemsNumber.forEach(function (item) {
+//       const check = item.querySelector('input[type=checkbox]')
+//       const label = item.querySelector('label')
+//       const input = item.querySelector('input[type=text]')
+
+//       console.log(check)
+//       console.log(label)
+//       console.log(input)
+//     })
+//     console.log(otherItemsPercent)
+//   },
+//метод запроса первоначальных данных
+//   asking: function () {
+//запрос наименования проекта и его проверка на string
+// do {
+//   appData.title = prompt('Как называется Ваш проект?')
+// } while (!appData.isString(appData.title))
+
+//цикл for используется для получения информации о типах экранов
+// for (let i = 0; i < 2; i++) {
+//   let name
+//   let answerPrice = 0
+//   //запрос наименований экранов и их проверка на string
+//   do {
+//     name = prompt('Какие типы экранов нужно разработать?')
+//   } while (!appData.isString(name))
+//   //запрос на стоимость разработок экранов и проверка на number
+//   do {
+//     answerPrice = prompt('Сколько будет стоить данная работа?')
+//   } while (!appData.isNumber(answerPrice))
+//   answerPrice = Number(answerPrice)
+//   //наполнение массива данных об экранах
+//   this.screens.push({ id: 1, name: name, price: answerPrice })
+// }
+
+//цикл for используется для получения информации о дополнительных сервисах
+//     for (let i = 0; i < 2; i++) {
+//       let answerPrice = 0
+//       let name
+//       //запрос наименования доуслуги и его проверка на string
+//       do {
+//         name = prompt('Какой дополнительный тип услуги №1 нужен?')
+//       } while (!appData.isString(name))
+//       //запрос на стоимость допуслуг и проверка на number
+//       do {
+//         answerPrice = prompt('Сколько это будет стоить?')
+//       } while (!appData.isNumber(answerPrice))
+//       answerPrice = Number(answerPrice)
+//       //наполнение объекта данными о допуслугах
+//       appData.services[name + i] = answerPrice
+//     }
+//   },
+
+//getPrices - метод отвечающий за расчёты цен
+
+// //метод проверки на number
+// isNumber: function (num) {
+//   return !isNaN(parseFloat(num)) && isFinite(num)
+// },
+
+// //метод проверки на string
+// isString: function (str) {
+//   return typeof str === 'string' && isNaN(Number(str))
+// },
+
+
+
+//   //метод получения отредактированного названия проекта
+//   getTitle: function () {
+//     let firstCharBig
+
+//     do {
+//       appData.title = appData.title.trim()
+//     } while (appData.title.indexOf(' ') === 0)
+
+//     appData.title = appData.title.toLowerCase()
+//     //первая буква в строке переводится в большой регистр
+//     firstCharBig = appData.title[0].toUpperCase()
+//     // методом replace изменяется первая строка title на firstCharBig
+//     appData.title = appData.title.replace(appData.title[0], firstCharBig)
+//   },
+
+
+
+//метод вычисления скидки
+// getRollBackMesssage: function (price) {
+//   switch (true) {
+//     case price >= 30000:
+//       return 'Даем скидку в 10%'
+//       break
+//     case price >= 15000 && price < 30000:
+//       return 'Даем скидку в 5%'
+//       break
+//     case price >= 0 && price < 15000:
+//       return 'Скидка не предусмотрена'
+//       break
+//     case price < 0:
+//       return 'Что то пошло не так'
+//       break
+//   }
+// },
+
+// logger: function () {
+//   console.log(appData.fullPrice)
+//   console.log(appData.servicePercentPrice)
+// },
